8ceb917a1d8a5202f5d5924605b8108a5ef1d757
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2023-12-06 14:45:04 +0300

    fix: add collections to our payload

--- src/server.ts
+++ src/server.ts
@@ -36,12 +36,12 @@ const start = async () => {
   app.use((req, res) => nextHandler(req, res));
 
   nexApp.prepare().then(() => {
-    // payload.logger.info('Next.js started');
+    payload.logger.info('Next.js started');
 
     app.listen(PORT, async () => {
-      // payload.logger.info(
-      //   `Next.js App Url: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-      // );
+      payload.logger.info(
+        `Next.js App Url: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
     });
   });
 };
