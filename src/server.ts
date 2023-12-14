29c9574b95064866930f4346ba6235d84181dd71
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2023-12-14 15:46:38 +0300

    feat: add listening for stripe webhook locally

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { nexApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
 import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -18,7 +20,15 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
